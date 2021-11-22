cf82b15f57dbd6ab60ea7b5ffdd3a0c31f935dc1
kamataryo <kamataryo@users.noreply.github.com>
2021-11-22 09:59:24 +0900

    Fix crawler

--- bin/build-banchigo.js
+++ bin/build-banchigo.js
@@ -18,7 +18,16 @@ readline
     const cityCode = quotedCityCode.replace(/"/g, '')
     const cityName = quotedCityName.replace(/"/g, '')
     if(!cityCodeMap.has(cityCode)) {
-      cityCodeMap.set(cityCode, { prefName, cityName })
+      if(
+        // cityCode === '01452' ||
+        // cityCode === '01455' ||
+        // cityCode === '25207' ||
+        cityCode === '44203' ||
+        cityCode === '47325'
+
+      ) { // TODO: debug
+        cityCodeMap.set(cityCode, { prefName, cityName })
+      }
     }
   } )
   .on('close',async () => {
@@ -26,9 +35,10 @@ readline
 
     for (const [cityCode, { prefName, cityName }] of cityCodeMap) {
       const cityUrl = urlFormat.replace('%code', cityCode)
+      console.log(cityUrl)
       const resp = await fetch(cityUrl)
-      let cyomokuCount = 0
-        if(resp.status === 200) {
+
+      if (resp.status === 200) {
         const { window } = new JSDOM(await resp.text())
         const anchors = window.document.getElementsByTagName('a')
         const cyomokuURLs = [...anchors]
@@ -36,7 +46,7 @@ readline
           .filter(link => link.match(new RegExp(`data/${cityCode}/${cityCode}_[0-9]+\.html`)))
           .map(relativeURL => url.resolve(cityUrl, relativeURL))
 
-        const banchigoItems = []
+        const banchigoItemsMap = {}
         while (cyomokuURLs.length > 0) {
           const cyomokuURL = cyomokuURLs.shift()
           console.log(cyomokuURL)
@@ -45,26 +55,39 @@ readline
           const table = window.document.getElementsByTagName('table')[0]
           const [header, ...rows] = table.querySelectorAll('tr')
           const headerTexts = [...header.querySelectorAll('th')].map(th => th.textContent)
-          cyomokuCount = rows.length
 
           for (const row of rows) {
             const cols = [...row.querySelectorAll('td')]
             const values = cols.map(td => td.textContent)
-            banchigoItems.push(values.reduce((prev, value, index) => {
+            const cyomokuName = values[headerTexts.indexOf('町又は字の名称')]
+            if(!banchigoItemsMap[cyomokuName]) {
+              banchigoItemsMap[cyomokuName] = []
+            }
+            banchigoItemsMap[cyomokuName].push(values.reduce((prev, value, index) => {
               prev[headerTexts[index]] = value
               return prev
             }, {}))
           }
-          await sleep(300)
+          await sleep(500)
         }
 
         const cityDirName = path.resolve(__dirname, '..', 'api', 'ja', prefName, cityName)
-        console.log(banchigoItems[0]['町又は字の名称'])
-        const banchigoFilename = path.resolve(cityDirName, banchigoItems[0]['町又は字の名称'] + '.json')
         fs.mkdirSync(cityDirName, { recursive: true })
-        fs.writeFileSync(banchigoFilename, JSON.stringify(banchigoItems))
+
+        const cyomokuNames = Object.keys(banchigoItemsMap)
+        for (const cyomokuName of cyomokuNames) {
+          const banchigoFilename = path.resolve(cityDirName, cyomokuName + '.json')
+          const data = banchigoItemsMap[cyomokuName].map(item => ({
+            gaiku: item['街区符号'],
+            kiso: item['基礎番号'],
+            lat: item['緯度(度単位10進数)'],
+            lng: item['経度(度単位10進数)'],
+          }))
+          fs.writeFileSync(banchigoFilename, JSON.stringify(data))
+        }
       }
-      cityCodeMap.set(cityCode, { prefName, cityName, status: resp.status, cyomokuCount })
+      cityCodeMap.set(cityCode, { prefName, cityName, status: resp.status })
+      await sleep(1000)
     }
 
     fs.writeFileSync(path.resolve(__dirname, 'result.json'), JSON.stringify(Object.fromEntries(cityCodeMap)))
